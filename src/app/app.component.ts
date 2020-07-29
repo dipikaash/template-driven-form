6bb097ccfd027b5cb42476e3fec6bff2e64e3b7f
Your Name <you@example.com>
2020-07-29 18:59:49 +0530

    td form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') f: NgForm;
   title = 'form-first';
+  genders =['male','female','others'];
+  submitted = false;
+   user = {
+    username :'',
+    email: '',
+    secretquestion: '',
+    gender: '',
+     };
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+  onSubmit(){
+    console.log(this.f);
+    this.submitted =true;
+    this.user.username = this.f.value.username;
+    this.user.email = this.f.value.email;
+    this.user.secretquestion = this.f.value.secret;
+    this.user.gender = this.f.value.gender;
+  }
 }
