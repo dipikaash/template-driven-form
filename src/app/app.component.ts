e0c9db0279381692bf49f2e8d1cb698b8e0b78a8
Your Name <you@example.com>
2020-07-30 15:37:01 +0530

    Used Reactive Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,22 @@
-import { Component,ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component,ViewChild, OnInit } from '@angular/core';
+import { FormGroup,FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  @ViewChild('f') f: NgForm;
-  title = 'form-first';
-  genders =['male','female','others'];
-  submitted = false;
-   user = {
-    username :'',
-    email: '',
-    secretquestion: '',
-    gender: '',
-     };
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-  }
-  onSubmit(){
-    console.log(this.f);
-    this.submitted =true;
-    this.user.username = this.f.value.username;
-    this.user.email = this.f.value.email;
-    this.user.secretquestion = this.f.value.secret;
-    this.user.gender = this.f.value.gender;
-  }
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+ f: FormGroup;
+ ngOnInit(){
+   this.f = new FormGroup({
+     'projectname': new FormControl(null,Validators.required),
+     'email': new FormControl(null,[Validators.required,Validators.email]),
+     'gender': new FormControl('female')
+   });
+ }
+ onSubmit(){
+   console.log(this.f);
+ }
 }
